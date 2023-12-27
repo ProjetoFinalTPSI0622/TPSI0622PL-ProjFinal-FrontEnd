a8e8f45e3b2f503828b1fbf23b4eec8801df3296
daniel pereira <danielpereira22costa@gmail.com>
2023-12-27 23:11:56 +0000

    color palette an router.js

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline text-red-500">
-      test! is working!
-  </h1>
+
+  <Navbar />
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
