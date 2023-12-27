a91134edf89db74d82a3bdb3e860b5176f3d4642
PedroNito <Pedro.nito.23@gmail.com>
2023-12-27 19:56:40 +0000

    Tailwind installation sucessefull

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-3xl font-bold underline text-red-500">
+      test! is working!
+  </h1>
 </template>
 
 <style scoped>
