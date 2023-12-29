bf069be97ebf5cf3c0a1fb2d4f6a8294f0f0dbb3
PedroNito <Pedro.nito.23@gmail.com>
2023-12-29 21:52:40 +0000

    Component OptionButton in OptionBar

---
+++ src/components/Settings/OptionButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button v-for="option in options"
+    class="flex flex-col items-center shadow-sm bg-white py-2.5 rounded-md md:items-start">
+    <div class="flex flex-col px-5 md:items-start">
+      <h1 class="text-primary text-2xl whitespace-nowrap">{{ option.name }}</h1>
+      <h3 class="text-primary text-sm whitespace-nowrap mt-2.5">{{ option.description }}</h3>
+    </div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'OptionsBar',
+  data() {
+    return {
+      options: [
+        {
+          name: 'Account Settings',
+          description: 'Details about your personal settings'
+        },
+        {
+          name: 'Notifications',
+          description: 'Details about your notifications system'
+        },
+        {
+          name: 'Password & Security',
+          description: 'Details about your password settings'
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
