8780f0fafee1c563bf3d02c8c539d91dcccb4ab2
PedroNito <Pedro.nito.23@gmail.com>
2024-01-06 00:22:17 +0000

    visual devices fix on page notifications

--- src/components/settings/notifications/OptionDescription.vue
+++ src/components/settings/notifications/OptionDescription.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full">
-        <div class="text-purple text-sm pl-2 md:pl-4 pr-4 lg:text-base">{{ textDescription }}</div>
+        <div class="text-purple text-sm pl-2 md:pl-4 pr-4 xl:text-lg">{{ textDescription }}</div>
     </div>
 </template>
 
