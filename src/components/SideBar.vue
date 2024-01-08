f6ed78ff830749dc88c57f87c657a9eca1fc666f
Fábio <fabiomiguel3.10@gmail.com>
2024-01-08 20:36:48 +0000

    FIX

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,9 +3,7 @@
     <div class="bg-purple flex flex-row sm:flex-col w-full h-14 sm:w-20 p-4 place-content-between">
         <div class="flex flex-row sm:flex-col gap-10">
             <img src="../assets/home.svg">
-          <router-link :to="{ name: 'ticket' }">
             <img src="../assets/ticket.svg" alt="ticket">
-          </router-link>
             <img src="../assets/user.svg">
             <img src="../assets/pie-chart.svg">
             <img class="" src="../assets/Ellipse 5.svg">
@@ -14,6 +12,5 @@
 </template>
 
 <script setup>
-import { router-link }  from 'vue-router'
 
 </script>
