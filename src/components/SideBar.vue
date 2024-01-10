6f5dc7f18286fbaef0a3f946cb6e76d084d070ab
PedroNito <Pedro.nito.23@gmail.com>
2024-01-10 19:56:31 +0000

    fixed sidebar in mobile

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="bg-purple flex flex-row w-full h-14 py-4 px-8 place-content-between absolute bottom-0 sm:flex-col sm:w-[4.5%] sm:h-full sm:p-4 sm:relative sm:place-content-start sm:gap-10">
+        class="bg-purple flex flex-row w-full h-14 py-4 px-8 place-content-between fixed bottom-0 sm:flex-col sm:w-[4.5%] sm:h-full sm:p-4 sm:relative sm:place-content-start sm:gap-10">
             <img src="../assets/home.svg">
             <img src="../assets/ticket.svg" alt="ticket">
             <img src="../assets/user.svg">
