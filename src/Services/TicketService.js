5623fdd06bcc38ac573b23fc52a8f42162bd370f
Fábio <fabiomiguel3.10@gmail.com>
2024-01-16 22:16:06 +0000

    TicketService fixed

--- src/Services/TicketService.js
+++ src/Services/TicketService.js
@@ -1,13 +1,79 @@
 import axios from 'axios';
 
-export const getTickets = async () => {
-    const response = await axios.get('http://127.0.0.1:8000/api/tickets',
-        {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            withCredentials: true,
-        });
-    return response.data;
+export const TicketService = {
+    getTickets: async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/tickets', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                withCredentials: true,
+            });
+            if (response.status === 200) {
+                return {success: true, message: 'Authenticated', tickets: response.data}
+            } else {
+                return {success: false, message: 'Not authenticated'}
+            }
+        } catch (e) {
+            return {success: false, message: 'Not authenticated'}
+        }
+    },
+    getTicket: async (id) => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/tickets/' + id, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                withCredentials: true,
+            });
+            console.log(response.data);
+            if (response.status === 200) {
+                return {success: true, message: 'Authenticated', ticket: response.data}
+            } else {
+                return {success: false, message: 'Not authenticated'}
+            }
+        } catch (e) {
+            return {success: false, message: 'Not authenticated'}
+        }
+    },
+    createTicket: async (title, description, priority, type) => {
+        try {
+            const response = await axios.post('http://localhost:8000/api/tickets', {
+                title: title,
+                description: description,
+                priority: priority,
+                type: type
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                withCredentials: true,
+            });
+            console.log(response.data);
+            if (response.status === 200) {
+                return {success: true, message: 'Authenticated', ticket: response.data}
+            } else {
+                return {success: false, message: 'Not authenticated'}
+            }
+        } catch (e) {
+            return {success: false, message: 'Not authenticated'}
+        }
+    },
+    updateTicket: async (id, title, description, priority, type) => {
+        try {
+            const response = await axios.put('http://localhost:8000/api/tickets/' + id, {
+                title: title,
+                description: description,
+                priority: priority,
+                type: type
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                withCredentials: true,
+            });
+        } catch (e) {
+            return {success: false, message: 'Not authenticated'}
+        }
+    }
 }
-
--- src/pages/ShowTicketPage.vue
+++ src/pages/ShowTicketPage.vue
@@ -1,15 +1,18 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { getTickets } from '../services/TicketService.js';
+import {ref, onMounted } from 'vue';
+import { TicketService } from '../Services/TicketService.js';
 import SideFilter from '../components/ShowTicket/SideFilter.vue';
 import TicketItem from '../components/ShowTicket/TicketItem.vue';
 import TopMenu from '../components/ShowTicket/TopMenu.vue';
 
 const tickets = ref([]);
 
+
 onMounted(async () => {
-    tickets.value = await getTickets();
+    tickets.value = (await TicketService.getTickets()).tickets;
+    await console.log(tickets.value);
 });
+
 </script>
 
 <template>
@@ -18,7 +21,7 @@ onMounted(async () => {
         <span class="flex flex-col w-full lg:w-[80%]">
             <TopMenu />
             <div class="flex flex-col flex-grow">
-                <TicketItem v-for="(ticket, index) in tickets" :key="index" :ticket="ticket" />
+                <TicketItem v-for="ticket in tickets" :ticket="ticket" />
             </div>
         </span>
     </div>
