9760082d5d34544050ee0a6b82690e265d6cca2e
PedroNito <Pedro.nito.23@gmail.com>
2024-01-17 20:07:43 +0000

    CreateUser Done

--- src/components/Form/ButtonSubmit.vue
+++ src/components/Form/ButtonSubmit.vue
@@ -1,8 +1,12 @@
 <template>
-    <button type="submit"
-        class="text-white text-center text-lg w-full font-bold justify-center items-center bg-purple py-3 rounded-md">
-        {{ textButton }}
-    </button>
+    <div class="flex justify-center">
+        <div class="flex items-center w-full xl:w-5/12">
+            <button type="submit"
+            class="text-white text-center text-lg w-full font-bold justify-center items-center bg-purple py-3 rounded-md">
+            {{ textButton }}
+        </button>
+    </div>
+</div>
 </template>
 
 <script>
