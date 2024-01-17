b384c6bec0086342180f02cd7b98d2c643e72f54
PedroNito <Pedro.nito.23@gmail.com>
2024-01-17 22:18:34 +0000

    deleting some old files

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -2,15 +2,18 @@
   <div class="md:flex w-full">
     <OptionsBar />
     <div class="flex flex-col h-fit w-full">
+      <EditUserForm />
     </div>
   </div>
 </template>
 
 <script>
   import OptionsBar from '../components/Settings/OptionsBar.vue';
+  import EditUserForm from '../components/settings/Account/EditUserForm.vue';
   export default {
     components: {
     OptionsBar,
+    EditUserForm,
   }
 }
 </script>
