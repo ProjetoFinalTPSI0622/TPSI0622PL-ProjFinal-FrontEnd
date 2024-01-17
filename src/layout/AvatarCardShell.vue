796ca8bbf193c6121f42954b10f8032a3a8665aa
PedroNito <Pedro.nito.23@gmail.com>
2024-01-17 01:02:26 +0000

    AvtarCaard done to Desktop

--- src/layout/AvatarCardShell.vue
+++ src/layout/AvatarCardShell.vue
@@ -1,5 +1,5 @@
 <template>
-        <div class="bg-grey rounded-md border-b-opacity-50 border-b m-5 lg:mx-10">
+        <div class="bg-grey rounded-md border-b-opacity-50 border-b m-5 lg:mx-10 xl:mx-0">
             <slot name="AvatarCard">
                 <slot></slot>
             </slot>
