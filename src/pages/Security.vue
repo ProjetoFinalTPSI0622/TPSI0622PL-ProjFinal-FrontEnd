67b6f1ad45098462ec1b6c7cf56556e50b2269a3
PedroNito <Pedro.nito.23@gmail.com>
2024-01-17 23:02:04 +0000

    visual fixing na bar settings

--- src/pages/Security.vue
+++ src/pages/Security.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="md:flex w-full">
-      <SettingsBar />
-      <div class="flex flex-col h-fit w-full">
-            <SecurityForm />
-      </div>
+  <div class="md:flex w-full">
+    <SettingsBar />
+    <div class="flex flex-col h-fit w-full">
+      <SecurityForm />
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-    import SettingsBar from '../components/Settings/SettingsBar.vue';
+<script>
+import SettingsBar from '../components/Settings/SettingsBar.vue';
 
-    import SecurityForm from '../components/settings/security/SecurityForm.vue';
-    export default {
-      components: {
+import SecurityForm from '../components/settings/security/SecurityForm.vue';
+export default {
+  components: {
     SettingsBar,
     SecurityForm,
-}
   }
-  </script>
+}
+</script>
 
