bdf0e1224a9d7cc26485c7732f453a640ec34145
PedroNito <Pedro.nito.23@gmail.com>
2024-01-22 23:36:52 +0000

    fixing createUser

--- src/components/Users/CreateUserForm.vue
+++ src/components/Users/CreateUserForm.vue
@@ -13,31 +13,31 @@
                     <Input LabelTitle="Email" type="email" required v-model="user.email" />
                 </div>
                 <div class="flex flex-col gap-5 mt-5 md:flex-row">
-                    <div class="flex flex-col md:flex-row md:items-end gap-3 lg:w-2/4">
+                    <!-- <div class="flex flex-col md:flex-row md:items-end gap-3 lg:w-2/4">
                         <Input LabelTitle="NIF" type="number" required v-model="userInfo.nif" />
                             <div class="flex flex-row mb-2 gap-2 ">
                                 <label class="text-purple text-sm">Set NIF as password</label>
                                 <input type="checkbox" v-model="isChecked" class="size-6">
                             </div>
-                    </div>
+                    </div> -->
 
                     <Input LabelTitle="Password" type="password" required v-model="user.password" />
                 </div>
                 <div class="flex flex-col gap-5 mt-5 md:flex-row">
                     <Input LabelTitle="Internal Code" type="text" required v-model="user.internalcode" />
-                    <Input LabelTitle="Phone Number" type="number" required v-model="userInfo.phoneNumber" />
-                    <Input LabelTitle="Birthday Date" type="date" required v-model="userInfo.birthdayDate" />
+                    <!-- <Input LabelTitle="Phone Number" type="number" required v-model="userInfo.phoneNumber" /> -->
+                    <!-- <Input LabelTitle="Birthday Date" type="date" required v-model="userInfo.birthdayDate" /> -->
                 </div>
-                <div class="flex flex-col gap-5 mt-5 md:flex-row ">
+                <!-- <div class="flex flex-col gap-5 mt-5 md:flex-row ">
                     <Input LabelTitle="Address" type="address" required v-model="userInfo.address" />
                     <Input LabelTitle="Zip Code" type="number" required v-model="userInfo.zipCode" />
                 </div>
                 <div class="flex flex-col gap-5 mt-5 md:flex-row md:mt-5">
                     <Input LabelTitle="State/Province" type="text" required v-model="userInfo.state" />
                     <Input LabelTitle="City" type="text" required v-model="userInfo.city" />
-                    <!-- <Input LabelTitle="Country" type="text" required v-model="userInfo.country" /> -->
+                    <Input LabelTitle="Country" type="text" required v-model="userInfo.country" />
                     <CountryDropdown/>
-                </div>
+                </div> -->
             </div>
 
             <ButtonSubmit textButton="Create User" />
@@ -46,13 +46,14 @@
 </template>
 
 <script>
-import axios from 'axios';
 import FormShell from '../../layout/FormShell.vue';
 import AvatarCard from '../Form/AvatarCard.vue';
 import FormTitle from '../../components/Form/FormTitle.vue';
 import Input from '../../components/Form/Input.vue';
 import CountryDropdown from '../../components/Form/CountryDropdown.vue';
 import ButtonSubmit from '../../components/Form/ButtonSubmit.vue';
+import { UserService } from '../../Services/UserService';
+
 export default {
     components: {
         FormShell,
@@ -60,7 +61,9 @@ export default {
         FormTitle,
         Input,
         CountryDropdown,
-        ButtonSubmit
+        ButtonSubmit,
+        UserService
+
     },
     data() {
         return {
@@ -71,17 +74,17 @@ export default {
                 password: '',
                 internalcode: '',
             },
-            userInfo: {
-                avatar: '',
-                nif: '',
-                phoneNumber: '',
-                birthdayDate: '',
-                address: '',
-                city: '',
-                state: '',
-                zipCode: '',
-                country: ''
-            }
+            // userInfo: {
+            //     avatar: '',
+            //     nif: '',
+            //     phoneNumber: '',
+            //     birthdayDate: '',
+            //     address: '',
+            //     city: '',
+            //     state: '',
+            //     zipCode: '',
+            //     country: ''
+            // }
         }
     },
     methods: {
@@ -108,23 +111,16 @@ export default {
                 //     country: this.userInfo.country
                 // },
             }
-            console.log(allData)
-            axios
-                .post('http://127.0.0.1:8000/api/user', allData.user,
-                    {
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        withCredentials: true,
-                    })
+            
+            
+            UserService.createUser(allData.user)
                 .then((response) => {
-                    console.log('User created', response.data)
-                    // this.userInfo.id = response.data.id;
-                    // this.$router.push('/users');
+                    console.log(response);
                 })
                 .catch((error) => {
-                    console.error('Error sending data:', error);
-                });
+                    console.log(error);
+                })
+                    
         },
     }
 }
