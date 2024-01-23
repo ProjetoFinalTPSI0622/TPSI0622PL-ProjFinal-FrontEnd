396abd011f625916ccc25b439be88e9808c35b7f
Goncalo Moreira <goncalo.moreira.t0123177@edu.atec.pt>
2024-01-23 20:35:10 +0000

    notifications on progress

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import VueCookies from 'vue-cookies';
 import { createPinia } from 'pinia';
 import FlagIcon from 'vue-flag-icon'
 import './style.css';
+import echo from './websockets';
+
 
 const pinia = createPinia();
 
@@ -14,4 +16,5 @@ app.use(FlagIcon);
 app.use(pinia);
 app.use(VueCookies);
 app.use(router);
+app.config.globalProperties.$echo = echo;
 app.mount('#app');
