3038bee127a22760308f59499be4d1d0d73eb3d9
Fábio <fabiomiguel3.10@gmail.com>
2024-01-30 02:27:25 +0000

    refactored services
    now using a config to avoid repeating code

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -1,64 +1,42 @@
 import axios from 'axios';
 
+const axiosConfig = {
+    baseURL: 'http://localhost:8000/api',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
 
 export const AuthService = {
-    login: async (email, password) => {
+    makeRequest: async (method, url, data) => {
         try {
-            const response = await axios.post('http://localhost:8000/api/auth/login', {
-                email: email,
-                password: password
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                withCredentials: true,
+            const response = await axios({
+                method,
+                url,
+                data,
+                ...axiosConfig,
             });
 
             if (response.status === 200) {
-                console.log(response.data);
-
-                return { success: true, message: 'Login successful' };
+                return { success: true, message: response.data.message, data: response.data };
             } else {
-                return { success: false, message: 'Login failed' };
+                return { success: false, message: response.data.message };
             }
         } catch (e) {
             return { success: false, message: 'An error occurred' };
         }
     },
 
-    checkAuth: async () => {
-        try {
-            const response = await axios.get('http://localhost:8000/api/auth/check',{
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                withCredentials: true,
-            });
-            if (response.status === 200 && response.data.auth) {
-                return {success: true, message: 'Authenticated'}
-            } else {
-                return {success: false, message: 'Not authenticated'}
-            }
-        } catch (e) {
-            return {success: false, message: 'Not authenticated'}
-        }
+    login: async (email, password) => {
+        return AuthService.makeRequest('post', '/auth/login', { email, password });
     },
+
+    checkAuth: async () => {
+        return AuthService.makeRequest('get', '/auth/check');
+    },
+
     userLogout: async () => {
-        try {
-            const response = await axios.get('http://localhost:8000/api/auth/logout',{
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                withCredentials: true,
-            });
-            console.log(response)
-            if (response.status === 200) {
-                return {success: true, message: 'Logged out'}
-            } else {
-                return {success: false, message: 'Not logged out'}
-            }
-        } catch (e) {
-            return {success: false, message: 'Not logged out'}
-        }
-    }
-}
+        return AuthService.makeRequest('get', '/auth/logout');
+    },
+};
--- src/main.js
+++ src/main.js
@@ -4,11 +4,24 @@ import router from './router';
 import VueCookies from 'vue-cookies';
 import { createPinia } from 'pinia';
 import './style.css';
+import axios from 'axios';
 // import echo from './websockets';
 
 const pinia = createPinia();
 
 const app = createApp(App);
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8000/api',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+
+app.config.globalProperties.$axios = instance;
+
 
 app.use(pinia);
 app.use(VueCookies);
