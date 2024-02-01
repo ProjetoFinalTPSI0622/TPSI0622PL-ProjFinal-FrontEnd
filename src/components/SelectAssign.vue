5698b50df97a0b96aa87228d8d18792cc16d3d72
daniel pereira <danielpereira22costa@gmail.com>
2024-02-01 23:04:27 +0000

    Added toasts

--- src/components/SelectAssign.vue
+++ src/components/SelectAssign.vue
@@ -1,3 +1,25 @@
+<script setup>
+import { defineProps, defineEmits, ref } from 'vue';
+
+const selectbox = ref(null);
+let oldValue = 0;
+
+defineProps({
+    assignedto: String,
+    technicians: Array
+});
+
+const emit = defineEmits(['show-modal']);
+
+const showTicketModal = (technicianName) => {
+    emit('show-modal', technicianName, selectbox, oldValue);
+};
+
+const handleMousedown = () => {
+    oldValue = selectbox.value.selectedIndex;
+};
+</script>
+
 <template>
     <select ref="selectbox" @change.prevent="showTicketModal($event.target.value)" @click.stop @mousedown="handleMousedown"
         class="border bg-white w-full py-1 lg:py-2 lg:px-2.5 rounded-lg border-solid border-black border-opacity-20">
@@ -10,25 +32,3 @@
         </option>
     </select>
 </template>
-
-<script setup>
-import { defineProps, defineEmits, ref } from 'vue';
-
-const selectbox = ref(null);
-let oldValue = 0;
-
-defineProps({
-    assignedto: String,
-    technicians: Array
-});
-
-const emit = defineEmits(['show-modal', 'selectbox']);
-
-const showTicketModal = (technicianName) => {
-    emit('show-modal', technicianName, selectbox, oldValue);
-};
-
-const handleMousedown = () => {
-    oldValue = selectbox.value.selectedIndex;
-};
-</script>
