d31dfc3e5324857695cc6dee8be3e216699ff1bd
Fábio <fabiomiguel3.10@gmail.com>
2024-02-06 00:46:57 +0000

    Build project

---
+++ public/assets/SimpleButton-nEiu8VTy.js
@@ -0,0 +1,1 @@
+import{_ as t,c as s,d as r,l as o}from"./index-Wf_wPvl6.js";const n={},c={class:"text-white text-lg sm:text-xl justify-center bg-purple py-0.5 px-3 rounded-2xl flex gap-2 w-fit cursor-pointer"};function l(e,a){return s(),r("span",c,[o(e.$slots,"default")])}const _=t(n,[["render",l]]);export{_ as S};
