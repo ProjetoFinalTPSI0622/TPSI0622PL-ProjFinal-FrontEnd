92a066b7a547e1af6ed94bb44841d1d0806cdb0b
Fábio <fabiomiguel3.10@gmail.com>
2024-02-20 00:13:22 +0000

    changed requests

--- src/Services/AxiosService.js
+++ src/Services/AxiosService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosConfig = {
-    baseURL: 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
     timeout: 30000,
 };
