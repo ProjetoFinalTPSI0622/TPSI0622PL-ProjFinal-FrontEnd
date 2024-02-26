4876baf8a32cf15e9541a92518e8bd2fee026f50
PedroNito <Pedro.nito.23@gmail.com>
2024-02-26 17:12:43 +0000

    sidebar mobile still not working

--- src/components/Navigation/SideBar.vue
+++ src/components/Navigation/SideBar.vue
@@ -4,22 +4,52 @@
 
         <router-link to="/" class="lg:w-full w-6"><img src="../../assets/home.svg"></router-link>
         <router-link to="/tickets/show" class="lg:w-full w-6"><img src="../../assets/ticket.svg"></router-link>
-        <router-link v-if="authedUserStore.userRole === 'admin'" to="/users" class="lg:w-full w-6"><img src="../../assets/user.svg"></router-link>
-        <router-link v-if="authedUserStore.userRole === 'admin' || authedUserStore.userRole === 'technician'" to="/dashboard" class="lg:w-full w-6"><img src="../../assets/pie-chart.svg"></router-link>
-        <router-link v-if="authedUserStore.userRole === 'admin'" to="/settings/configurations" class="lg:w-full w-6"><img src="../../assets/settings.svg"></router-link>
-        <router-link to="/settings/account" class="lg:w-full w-6 md:hidden min-w-4"><img src="../../assets/Ellipse 5.svg"></router-link>
+        <router-link v-if="authedUserStore.userRole === 'admin'" to="/users" class="lg:w-full w-6"><img
+                src="../../assets/user.svg"></router-link>
+        <router-link v-if="authedUserStore.userRole === 'admin' || authedUserStore.userRole === 'technician'"
+            to="/dashboard" class="lg:w-full w-6"><img src="../../assets/pie-chart.svg"></router-link>
+        <router-link v-if="authedUserStore.userRole === 'admin'" to="/settings/configurations" class="lg:w-full w-6"><img
+                src="../../assets/settings.svg"></router-link>
+        
+        <div class="lg:w-full w-6 md:hidden min-w-4" ref="secondIconRef" @click.stop="toggleSecondDropdown">
+            <img src="../../assets/Ellipse 5.svg">
+            <div v-if="showSecondDropdown" @click.stop="toggleSecondDropdown" ref="secondDropdownRef"
+                class="absolute top-full right-0 mt-2.5 py-2 w-44 bg-white rounded-md shadow-xl z-20 border border-black border-opacity-20">
+                <ul class="block text-right">
+                    <li v-for="item in dropdownItems" :key="item.id">
+                        <router-link :to="{ name: item.routeName }" class="block px-3 py-2 text-gray-700 hoverBlue">
+                            {{ item.name }}
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router';
-import { onBeforeMount } from 'vue';
 import { useAuthedUserStore } from '@/Stores/UserStore.js';
+import { ref, reactive, onBeforeMount } from 'vue';
 
 const authedUserStore = useAuthedUserStore();
+const showSecondDropdown = ref(false);
+const secondDropdownRef = ref(null);
+const secondIconRef = ref(null);
+
 
 onBeforeMount(async () => {
     await authedUserStore.fetchAuthedUser();
 });
+
+const dropdownItems = reactive([
+    { id: 1, name: 'Definições de conta', routeName: 'Account' },
+    { id: 2, name: 'Terminar sessão', routeName: 'Logout' }
+]);
+
+const toggleSecondDropdown = () => {
+    showSecondDropdown.value = !showSecondDropdown.value;
+};
 
 </script>
